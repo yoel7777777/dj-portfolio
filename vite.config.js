86b9ad436cc565d71750a9796e829ef9ed873ef9
yoel7777777 <yoeltarigan2000@gmail.com>
2025-08-09 22:53:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// GANTI "dj-portfolio" dgn nama repo kamu
+export default defineConfig({
+  plugins: [react()],
+  base: "/dj-portfolio/",
+});
